282abbc20ae27789e913243e132fa455a8dcf1b5
SprinterGameDev <sprinter@ozersk.com>
2018-02-26 16:13:43 +0500

    travis & coverage.

---
+++ remove-bitcore-lib-dep.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const Path = require('path');
+
+const pathsToClean = [
+    Path.resolve('./node_modules/bitcore-ecies/node_modules/bitcore-lib'),
+    Path.resolve('./node_modules/bitcore-message/node_modules/bitcore-lib'),
+];
+
+(function () {
+    function deleteFolderRecursive(path) {
+        console.log(path, fs.existsSync(path));
+        if (fs.existsSync(path)) {
+            fs.readdirSync(path).forEach(function (file, index) {
+                const curPath = path + "/" + file;
+                if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                    deleteFolderRecursive(curPath);
+                } else { // delete file
+                    fs.unlinkSync(curPath);
+                }
+            });
+            fs.rmdirSync(path);
+        }
+    }
+
+    for (let path of pathsToClean) {
+        deleteFolderRecursive(path);
+    }
+})();
