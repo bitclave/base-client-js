31be694c8a2db5d8a3d21235b7684dfb45cd302c
SprinterGameDev <sprinter@ozersk.com>
2018-05-21 12:53:12 +0500

    add client id filed to offer share data model.

--- src/repository/models/OfferShareData.ts
+++ src/repository/models/OfferShareData.ts
@@ -1,6 +1,7 @@
 export default class OfferShareData {
     offerSearchId: number = 0;
     offerOwner: string = '0x0';
+    clientId: string = '0x0';
     clientResponse: string = '';
     worth: string = '0';
     accepted: boolean = false;
