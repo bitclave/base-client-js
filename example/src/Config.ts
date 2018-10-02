9932a8b410141ce68b551c4283d31795d371a279
Anton Batalin <antbat2009@gmail.com>
2018-10-02 19:22:34 +0300

    update dist (lib)

--- example/src/Config.ts
+++ example/src/Config.ts
@@ -3,9 +3,9 @@ export default class Config {
     private static isDebug: boolean = true;
 
     public static getBaseEndPoint(): string {
-        // return this.isDebug ? 'https://base2-bitclva-com.herokuapp.com' : '';
+        return this.isDebug ? 'https://base2-bitclva-com.herokuapp.com' : '';
         // return this.isDebug ? 'https://base-node-staging.herokuapp.com' : '';
-        return this.isDebug ? 'http://localhost:8080' : '';
+        // return this.isDebug ? 'http://localhost:8080' : '';
     }
 
     public static getSignerEndPoint(): string {
