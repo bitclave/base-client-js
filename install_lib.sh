4154d2ac203268fed08d93c30078001dd42abc29
Mark Shwartzman <mark@bitclave.com>
2018-12-19 20:02:33 -0800

    remove example from git as it is generated as part of the build

--- install_lib.sh
+++ install_lib.sh
@@ -4,14 +4,28 @@ pushd .
 # cleanup lib folder
 rm -rf ./lib/*
 rm -rf dist
-rm -rf example/public/base-lib/bitclave-base/Bitclave-Base.*
+rm -rf example/public
 
 npm install       #postinstall: node remove-bitcore-lib-dep.js
 npm run preBuild  #tsc --outDir dist -d
 npm run build     # webpack
 
+mkdir -p example/public/base-lib/bitclave-base
+mkdir -p example/public/base-lib/@types/bitclave-base
+
+
 cp ./dist/Bitclave-Base.js* example/public/base-lib/bitclave-base
 cp -R ./dist/lib/* example/public/base-lib/@types/bitclave-base
 mv ./example/public/base-lib/@types/bitclave-base/Base.d.ts ./example/public/base-lib/@types/bitclave-base/index.d.ts
+cp example/package.json example/public/base-lib/bitclave-base/
+
+cd base-client-js && make build && cd ..
+
+cp -R ./dist/docs ./lib
+cp -R ./base-client-js/dist/BitclaveBase.* ./lib
+
+rm -rf example/public
+
+echo base-client-js is ready for publishing
 
 popd
