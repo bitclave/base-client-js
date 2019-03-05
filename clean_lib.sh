d39a8d09f185885031b34ed931446c8f3288b4a2
Mark Shwartzman <mark@bitclave.com>
2019-03-04 17:56:39 -0800

    remove unused scripts

--- clean_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-#git checkout dist
-
-rm -rf example/public
-#git checkout example/public/base-lib/bitclave-base
-
--- fill_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd .
-
-# fill up the libarary folder
-cp -R ./dist/docs ./lib
-cp -R ./base-client-js/dist/BitclaveBase.* ./lib
-
-popd
