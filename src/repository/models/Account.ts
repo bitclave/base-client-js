c0b45b07769f24142bf34491bb1bc34840226c02
AndreyShashlov <andrey@bitclave.com>
2019-04-23 10:36:33 +0300

    add createdAt/updatedAt for Account model

--- src/repository/models/Account.ts
+++ src/repository/models/Account.ts
@@ -1,3 +1,4 @@
+import { JsonObject } from './JsonObject';
 import SimpleAccount from './SimpleAccount';
 
 export default class Account extends SimpleAccount {
@@ -5,7 +6,10 @@ export default class Account extends SimpleAccount {
     public message: string = '';
     public sig: string = '';
 
-    public static fromJson(json: object): Account {
+    public static fromJson(json: JsonObject<Account>): Account {
+        json.createdAt = new Date((json.createdAt as string) || new Date().getTime());
+        json.updatedAt = new Date((json.updatedAt as string) || new Date().getTime());
+
         return Object.assign(new Account(), json);
     }
 
@@ -15,5 +19,17 @@ export default class Account extends SimpleAccount {
 
     public toSimpleAccount() {
         return new SimpleAccount(this.publicKey, this.nonce);
+    }
+
+    public toJson(): object {
+        const json = JSON.parse(JSON.stringify(this));
+        json.createdAt = this.createdAt.toJSON();
+        json.updatedAt = this.updatedAt.toJSON();
+
+        return json;
+    }
+
+    protected deepCopyFromJson(): SimpleAccount {
+        return Account.fromJson(this.toJson() as JsonObject<Account>);
     }
 }
