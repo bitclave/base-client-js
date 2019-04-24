158a86934fa84147f12be57b1f96f8e0071ea88e
AndreyShashlov <andrey@bitclave.com>
2019-04-24 11:48:56 +0300

    clean up spaces in code

--- src/manager/ProfileManagerImpl.ts
+++ src/manager/ProfileManagerImpl.ts
@@ -56,9 +56,9 @@ export class ProfileManagerImpl implements ProfileManager {
 
     public getData(fieldKey?: string | Array<string>): Promise<Map<string, string>> {
         if (
-            this.account == null || 
-            this.account.publicKey == null || 
-            this.account.publicKey === undefined || 
+            this.account == null ||
+            this.account.publicKey == null ||
+            this.account.publicKey === undefined ||
             this.account.publicKey.length < 1) {
             this.logger.error(`publicKey is not  found in base-client-js`);
             throw new Error('publicKey can not find');
