86b8586201b263f776e3b3f44b1602a5e4f03a37
Anton Batalin <antbat2009@gmail.com>
2019-05-10 15:47:27 +0300

    add offerRank model

---
+++ src/repository/models/OfferRank.ts
@@ -0,0 +1,21 @@
+export class OfferRank {
+    public id: number;
+    public rank: number;
+    public offerId: number;
+    public rankerId: number;
+    public createdAt?: Date;
+    public updatedAt?: Date;
+
+    // tslint:disable-next-line: no-any
+    constructor(obj?: any) {
+        if (obj) {
+            this.id = obj.id;
+            this.rank = obj.rank;
+            this.offerId = obj.offerId;
+            this.rankerId = obj.rankerId;
+
+            this.createdAt = obj.createdAt;
+            this.updatedAt = obj.updatedAt;
+        }
+    }
+}
