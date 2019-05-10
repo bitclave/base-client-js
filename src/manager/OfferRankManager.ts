45bf63fff9d5c5f433f78ae73e3e99c2b052d818
Anton Batalin <antbat2009@gmail.com>
2019-05-10 17:51:52 +0300

    OfferRank manager

---
+++ src/manager/OfferRankManager.ts
@@ -0,0 +1,13 @@
+import { OfferRank } from './../repository/models/OfferRank';
+
+export interface OfferRankManager {
+
+    getByOfferIdAndRankId(offerId: number, rankerId: number): Promise<OfferRank>;
+    getByOfferId(offerId: number): Promise<Array<OfferRank>>;
+    getById(offerRankId: number): Promise<OfferRank>;
+
+    create(rank: number, rankerId: number, offerId: number): Promise<OfferRank>;
+    update(offerRank: OfferRank): Promise<OfferRank>;
+
+    delete(offerRankId: number): Promise<number>;
+}
