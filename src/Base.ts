4d36ef39037322871e1448bff00277d95bfd5408
Anton Batalin <antbat2009@gmail.com>
2019-05-10 17:52:45 +0300

    add OfferRankManager to Base

--- src/Base.ts
+++ src/Base.ts
@@ -99,6 +99,10 @@ export { AcceptedField } from './utils/keypair/AcceptedField';
 export { RpcToken } from './utils/keypair/rpc/RpcToken';
 export { RpcAuth } from './utils/keypair/rpc/RpcAuth';
 
+import { OfferRankManager } from './manager/OfferRankManager';
+import { OfferRankManagerImpl } from './manager/OfferRankManagerImpl';
+import { OfferRankRepositoryImpl } from './repository/offerRank/OfferRankRepositoryImpl';
+
 export {
     CryptoWallets,
     EthWalletData,
@@ -175,6 +179,7 @@ export default class Base {
     private readonly _externalServicesManager: ExternalServicesManager;
     private readonly _authAccountBehavior: BehaviorSubject<Account> = new BehaviorSubject<Account>(new Account());
     private readonly _repositoryStrategyInterceptor: RepositoryStrategyInterceptor;
+    private readonly _offerRankManager: OfferRankManager;
 
     constructor(
         nodeHost: string,
@@ -215,6 +220,7 @@ export default class Base {
         const offerSearchRepository: OfferSearchRepository = new OfferSearchRepositoryImpl(transport);
         const verifyRepository: VerifyRepository = new VerifyRepositoryImpl(transport);
         const externalServicesRepository: ExternalServicesRepository = new ExternalServicesRepositoryImpl(transport);
+        const offerRankRepository = new OfferRankRepositoryImpl(transport);
 
         this._accountManager = new AccountManagerImpl(
             accountRepository,
@@ -260,6 +266,7 @@ export default class Base {
         );
 
         this._externalServicesManager = new ExternalServicesManagerImpl(externalServicesRepository);
+        this._offerRankManager = new OfferRankManagerImpl(offerRankRepository);
     }
 
     public changeStrategy(strategy: RepositoryStrategyType) {
@@ -296,6 +303,9 @@ export default class Base {
 
     get externalServicesManager(): ExternalServicesManager {
         return this._externalServicesManager;
+    }
+    get offerRankManager(): OfferRankManager {
+        return this._offerRankManager;
     }
 
     private createNodeAssistant(httpTransport: HttpTransport): AssistantNodeRepository {
