c5e9372ac15c99028667f0aa74bf487b1c5e32b5
Anton Batalin <antbat2009@gmail.com>
2019-05-16 14:55:32 +0300

    skip OfferRank test until Base-node server old

--- test/offerRank/offerRankTest.ts
+++ test/offerRank/offerRankTest.ts
@@ -21,8 +21,7 @@ async function createUser(user: Base, pass: string): Promise<Account> {
         await user.accountManager.authenticationByAccessToken(accessToken, someSigMessage);
         await user.accountManager.unsubscribe();
     } catch (e) {
-        // console.log('check createUser', e);
-        // ignore error if user not exist
+        // ignore
     }
 
     return await user.accountManager.registration(pass, someSigMessage); // this method private.
@@ -30,7 +29,7 @@ async function createUser(user: Base, pass: string): Promise<Account> {
 function randomOfferId() {
     return (new Date()).getTime();
 }
-
+/*
 describe('OfferRank', async () => {
     const base = new Base(
         baseNodeUrl,
@@ -39,73 +38,51 @@ describe('OfferRank', async () => {
         rpcSignerHost
     );
     it('should create simple offer', async () => {
-        try {
-            await createUser(base, 'OfferRankPassTest');
-            const rank = 11;
-            const baseOfferId = randomOfferId();
-            const RANKER = 1;
-            const offerRank = await base.offerRankManager.create(rank, RANKER, baseOfferId);
-            offerRank.should.exist;
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
+        await createUser(base, 'OfferRankPassTest');
+        const rank = 11;
+        const baseOfferId = randomOfferId();
+        const RANKER = 1;
+        const offerRank = await base.offerRankManager.create(rank, RANKER, baseOfferId);
+        offerRank.should.exist;
     });
     it('should create new OfferRank via update', async () => {
-        try {
-            await createUser(base, 'OfferRankPassTest');
-            const rank = 112;
-            const offerId = randomOfferId();
-            const rankerId = 1;
-            const offerRank = new OfferRank({rank, offerId, rankerId});
-            const created = await base.offerRankManager.update(offerRank);
-            created.id.should.exist;
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
+        await createUser(base, 'OfferRankPassTest');
+        const rank = 112;
+        const offerId = randomOfferId();
+        const rankerId = 1;
+        const offerRank = new OfferRank({rank, offerId, rankerId});
+        const created = await base.offerRankManager.update(offerRank);
+        created.id.should.exist;
     });
     it('should update created OfferRank via update', async () => {
-        try {
-            const offerId = randomOfferId();
-            const rankerId = 1;
+        const offerId = randomOfferId();
+        const rankerId = 1;
 
-            await createUser(base, 'OfferRankPassTest');
-            const offerRank = await base.offerRankManager.create(11, rankerId, offerId);
-            const id = offerRank.id;
-            const createdAt = offerRank.createdAt;
-            const updatedAt = offerRank.updatedAt;
+        await createUser(base, 'OfferRankPassTest');
+        const offerRank = await base.offerRankManager.create(11, rankerId, offerId);
+        const id = offerRank.id;
+        const createdAt = offerRank.createdAt;
+        const updatedAt = offerRank.updatedAt;
 
-            const rank = 112;
-            const forUpdating = new OfferRank({id, rank, offerId, rankerId, createdAt, updatedAt});
+        const rank = 112;
+        const forUpdating = new OfferRank({id, rank, offerId, rankerId, createdAt, updatedAt});
 
-            const updated = await base.offerRankManager.update(forUpdating);
-            updated.id.should.be.eql(id);
-            updated.rank.should.be.eql(112);
-            updated.createdAt.should.be.eql(createdAt);
-            updated.updatedAt.should.not.be.eql(updatedAt);
-
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
+        const updated = await base.offerRankManager.update(forUpdating);
+        updated.id.should.be.eql(id);
+        updated.rank.should.be.eql(112);
+        updated.createdAt.should.be.eql(createdAt);
+        updated.updatedAt.should.not.be.eql(updatedAt);
     });
     it('should get OfferRanks by offerId', async () => {
-        try {
-            await createUser(base, 'OfferRankPassTest');
-            const offerId = randomOfferId();
+        await createUser(base, 'OfferRankPassTest');
+        const offerId = randomOfferId();
 
-            await base.offerRankManager.create(11, 1, offerId);
-            await base.offerRankManager.create(22, 2, offerId);
-            await base.offerRankManager.create(33, 3, offerId);
+        await base.offerRankManager.create(11, 1, offerId);
+        await base.offerRankManager.create(22, 2, offerId);
+        await base.offerRankManager.create(33, 3, offerId);
 
-            const offerRanks = await base.offerRankManager.getByOfferId(offerId);
-            offerRanks.length.should.be.eql(3);
-
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
+        const offerRanks = await base.offerRankManager.getByOfferId(offerId);
+        offerRanks.length.should.be.eql(3);
     });
     it('should not allow change OfferRank offerId', async () => {
         try {
@@ -136,20 +113,16 @@ describe('OfferRank', async () => {
         }
     });
     it('should update OfferRanks if exist OfferRank with the same OfferIf and RankerId', async () => {
-        try {
-            await createUser(base, 'OfferRankPassTest');
-            const offerId = randomOfferId();
+        await createUser(base, 'OfferRankPassTest');
+        const offerId = randomOfferId();
 
-            await base.offerRankManager.create(11, 1, offerId);
-            await base.offerRankManager.create(22, 1, offerId);
+        await base.offerRankManager.create(11, 1, offerId);
+        await base.offerRankManager.create(22, 1, offerId);
 
-            const offerRanks = await base.offerRankManager.getByOfferId(offerId);
-            offerRanks.length.should.be.eql(1);
-            offerRanks[0].rank.should.be.eql(22);
-
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
+        const offerRanks = await base.offerRankManager.getByOfferId(offerId);
+        offerRanks.length.should.be.eql(1);
+        offerRanks[0].rank.should.be.eql(22);
     });
 });
+
+*/
