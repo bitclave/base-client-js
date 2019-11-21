8094684aa69eb89f8bcfcd92c11bb31c968b3a69
AndreyShashlov <andrey@bitclave.com>
2019-11-21 16:39:13 +0300

    removed redundant code.

--- src/utils/types/json-transform/deserializers/AccessRightMapDeserializer.ts
+++ src/utils/types/json-transform/deserializers/AccessRightMapDeserializer.ts
@@ -5,9 +5,6 @@ export class AccessRightMapDeserializer implements JsonDeserializer<Map<string, 
 
     public fromJson(json: JsonObject<Map<string, AccessRight>>): Map<string, AccessRight> {
         // @ts-ignore
-        const result = new Map<string, AccessRight>(Object.entries(json));
-        result.forEach((value: number, key: object) => result.set(key, value));
-
-        return result;
+        return new Map<string, AccessRight>(Object.entries(json));
     }
 }
