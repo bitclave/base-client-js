ba86d1d2385a3a418b5ff7e453676a00db2f9f9d
AndreyShashlov <andrey@bitclave.com>
2019-11-21 16:29:42 +0300

    JsonRpc fully described request/response model.

--- src/repository/source/rpc/JsonRpc.ts
+++ src/repository/source/rpc/JsonRpc.ts
@@ -2,12 +2,35 @@ export class JsonRpc {
 
     public readonly jsonrpc: string = '2.0';
     public readonly method: string;
-    public readonly params: Array<object>;
     public readonly id: number;
+    public readonly params: Array<object>;
+    public readonly result?: object | Array<object>;
+    public readonly error?: object | Array<object>;
 
-    constructor(method: string, params: Array<object>, id: number) {
-        this.method = method;
-        this.params = params;
+    public static request(id: number, method: string, params: Array<object>): JsonRpc {
+        return new JsonRpc(id, method, params);
+    }
+
+    public static response(
+        id: number,
+        method: string,
+        result?: object | Array<object>,
+        error?: object | Array<object>
+    ): JsonRpc {
+        return new JsonRpc(id, method, undefined, result, error);
+    }
+
+    private constructor(
+        id: number,
+        method: string,
+        params: Array<object> = [],
+        result?: object | Array<object>,
+        error?: object | Array<object>
+    ) {
         this.id = id;
+        this.method = method;
+        this.params = params || [];
+        this.result = result;
+        this.error = error;
     }
 }
