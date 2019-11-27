9e24eaf0b8eb3e40272343fde7f07dbe59eb5201
AndreyShashlov <andrey@bitclave.com>
2019-11-27 15:12:56 +0300

    fix Page deserializer.

--- src/utils/types/json-transform/deserializers/PageDeserializer.ts
+++ src/utils/types/json-transform/deserializers/PageDeserializer.ts
@@ -1,8 +1,9 @@
 import { JsonObject } from '../../../../repository/models/JsonObject';
+import { JsonTransform } from '../../../../repository/models/JsonTransform';
 import { Page } from '../../../../repository/models/Page';
 import { JsonDeserializer } from '../JsonDeserializer';
 
-export class PageDeserializer<T> implements JsonDeserializer<Page<T>> {
+export class PageDeserializer<T extends JsonTransform> implements JsonDeserializer<Page<T>> {
 
     // tslint:disable-next-line:callable-types
     constructor(private readonly Creator: { new(): T; }) {
